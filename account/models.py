c1c684fdef8ab5637152dbe8b0fb3289e655e147
Timi <tunmisebabade@gmail.com>
2024-07-15 17:30:47 +0100

    first commit

--- account/models.py
+++ account/models.py
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
             lastname=lastname,
             **extra_fields
         )
-        # user.set_password(password)
+        user.password = password
         user.save()
         return user
     
