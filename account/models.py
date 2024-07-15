751dd8148e9297514d0f4e4c3e6a61df23b086e0
Timi <tunmisebabade@gmail.com>
2024-07-15 18:07:45 +0100

    first commit

--- account/models.py
+++ account/models.py
@@ -38,7 +38,7 @@ class USERS(AbstractBaseUser, PermissionsMixin):
     created_at = models.DateTimeField(auto_now=True,null=True)
     updated_at = models.DateTimeField(auto_now_add=True, null=True)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     wallet = models.IntegerField(default=0)
 
     objects = UserManager()
